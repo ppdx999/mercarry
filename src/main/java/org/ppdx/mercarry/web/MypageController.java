d39a3ed6ccee7764ef1ecee8f8e850766bde69a6
fujis <ppdx999@gmail.com>
2024-09-16 19:58:58 +0900

    implement mypage's product list page

--- src/main/java/org/ppdx/mercarry/web/MypageController.java
+++ src/main/java/org/ppdx/mercarry/web/MypageController.java
@@ -3,12 +3,25 @@ package org.ppdx.mercarry.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.ppdx.mercarry.product.service.ProductService;
+import org.ppdx.mercarry.user.domain.User;
 
 @Controller
 public class MypageController extends BaseController {
 
+	@Autowired
+	private ProductService productService;
+
 	@GetMapping("/mypage")
 	public String mypage(Model model) {
 		return "mypage/index";
+	}
+
+	@GetMapping("/mypage/products")
+	public String mypageProducts(Model model, @AuthenticationPrincipal User user) {
+		model.addAttribute("products", productService.getProductsBySupplier(user));
+		return "mypage/products";
 	}
 }
