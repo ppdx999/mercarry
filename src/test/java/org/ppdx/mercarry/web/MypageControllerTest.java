1b865910662f36362bba5cd18b91df61f6480445
fujis <ppdx999@gmail.com>
2024-09-20 14:29:33 +0900

    write product create form test

--- src/test/java/org/ppdx/mercarry/web/MypageControllerTest.java
+++ src/test/java/org/ppdx/mercarry/web/MypageControllerTest.java
@@ -1,6 +1,7 @@
 package org.ppdx.mercarry.web;
 
 import org.junit.jupiter.api.Test;
+import org.ppdx.mercarry.product.service.ProductService;
 import org.ppdx.mercarry.user.service.CustomUserDetailsService;
 import org.ppdx.mercarry.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,9 +12,15 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.security.test.context.support.WithMockUser;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -31,6 +38,9 @@ public class MypageControllerTest {
 		@MockBean
 		private BCryptPasswordEncoder passwordEncoder;
 
+		@MockBean
+		private ProductService productService;
+
 		@Test
 		@WithMockUser
 		void testMypagePage() throws Exception {
@@ -44,4 +54,30 @@ public class MypageControllerTest {
 				mockMvc.perform(get("/mypage"))
 								.andExpect(status().is3xxRedirection());
 		}
+
+    @Test
+    @WithMockUser
+    public void testCreateProductSuccess() throws Exception {
+        mockMvc.perform(post("/mypage/products")
+                .param("name", "Sample Product")
+                .param("price", "1000")
+								.with(csrf())
+        )
+        .andExpect(status().is3xxRedirection())
+        .andExpect(redirectedUrl("/mypage/products"));
+    }
+
+		@Test
+		@WithMockUser
+		public void testCreateProductWithInvalidParams() throws Exception {
+				mockMvc.perform(post("/mypage/products")
+								.param("name", "Sample Product")
+								.param("price", "-1")
+								.with(csrf())
+				)
+				.andExpect(status().isOk())
+				.andExpect(view().name("mypage/products/new"))
+				.andExpect(model().attributeHasFieldErrors("product", "price"));
+		}
+
 }
