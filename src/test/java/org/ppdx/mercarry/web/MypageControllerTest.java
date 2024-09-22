eca1cca3550b663d3242ea17d4dbc71bc46081ac
fujis <ppdx999@gmail.com>
2024-09-22 21:43:04 +0900

    format

--- src/test/java/org/ppdx/mercarry/web/MypageControllerTest.java
+++ src/test/java/org/ppdx/mercarry/web/MypageControllerTest.java
@@ -23,75 +23,71 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class MypageControllerTest {
-		@Autowired
-		private MockMvc mockMvc;
+	@Autowired
+	private MockMvc mockMvc;
 
-		@MockBean
-		private UserService userService;
+	@MockBean
+	private UserService userService;
 
-		@MockBean
-		private CustomUserDetailsService customUserDetailsService;
+	@MockBean
+	private CustomUserDetailsService customUserDetailsService;
 
-		@MockBean
-		private BCryptPasswordEncoder passwordEncoder;
+	@MockBean
+	private BCryptPasswordEncoder passwordEncoder;
 
-		@MockBean
-		private ProductService productService;
+	@MockBean
+	private ProductService productService;
 
-		@Test
-		@WithMockUser
-		void testMypagePage() throws Exception {
-				mockMvc.perform(get("/mypage"))
-								.andExpect(status().isOk())
-								.andExpect(view().name("mypage/index"));
-		}
+	@Test
+	@WithMockUser
+	void testMypagePage() throws Exception {
+		mockMvc.perform(get("/mypage"))
+				.andExpect(status().isOk())
+				.andExpect(view().name("mypage/index"));
+	}
 
-		@Test
-		void testMypagePageWhenNotLoggedIn() throws Exception {
-				mockMvc.perform(get("/mypage"))
-								.andExpect(status().is3xxRedirection());
-		}
+	@Test
+	void testMypagePageWhenNotLoggedIn() throws Exception {
+		mockMvc.perform(get("/mypage"))
+				.andExpect(status().is3xxRedirection());
+	}
 
-    @Test
-    @WithMockUser
-    public void testCreateProductSuccess() throws Exception {
-        mockMvc.perform(post("/mypage/products")
-                .param("name", "Sample Product")
-                .param("price", "1000")
-								.with(csrf())
-        )
-        .andExpect(status().is3xxRedirection())
-        .andExpect(redirectedUrl("/mypage/products"));
-    }
+	@Test
+	@WithMockUser
+	public void testCreateProductSuccess() throws Exception {
+		mockMvc.perform(post("/mypage/products")
+				.param("name", "Sample Product")
+				.param("price", "1000")
+				.with(csrf()))
+				.andExpect(status().is3xxRedirection())
+				.andExpect(redirectedUrl("/mypage/products"));
+	}
 
-		@WithMockUser
-		@ParameterizedTest
-		@ValueSource(ints = {-1, 99, 50001})
-		public void testCreateProductFailWithOutOf100To50000Price(int price) throws Exception {
-				mockMvc.perform(post("/mypage/products")
-								.param("name", "Sample Product")
-								.param("price", String.valueOf(price))
-								.with(csrf())
-				)
+	@WithMockUser
+	@ParameterizedTest
+	@ValueSource(ints = { -1, 99, 50001 })
+	public void testCreateProductFailWithOutOf100To50000Price(int price) throws Exception {
+		mockMvc.perform(post("/mypage/products")
+				.param("name", "Sample Product")
+				.param("price", String.valueOf(price))
+				.with(csrf()))
 				.andExpect(status().isOk())
 				.andExpect(view().name("mypage/products/new"))
 				.andExpect(model().attributeHasFieldErrors("product", "price"));
-		}
+	}
 
-		@Test
-		@WithMockUser
-		public void testCreateProductFailWithEmptyName() throws Exception {
-				mockMvc.perform(post("/mypage/products")
-								.param("name", "")
-								.param("price", "3000")
-								.with(csrf())
-				)
+	@Test
+	@WithMockUser
+	public void testCreateProductFailWithEmptyName() throws Exception {
+		mockMvc.perform(post("/mypage/products")
+				.param("name", "")
+				.param("price", "3000")
+				.with(csrf()))
 				.andExpect(status().isOk())
 				.andExpect(view().name("mypage/products/new"))
 				.andExpect(model().attributeHasFieldErrors("product", "name"));
-		}
+	}
 }
